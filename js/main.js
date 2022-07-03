2f3548da80e80379ade14cef39b65bd73dffbaa1
a@b.c <a@b.c>
2022-07-03 17:45:28 +0500

    d

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+var Content;
+
 window.onload = function(e){
   window.alert('Hello!');
-  content = document.getElementById('content');
-  content.innerHTML = 'Hello! You are welcome.';
+  Content = document.getElementById('content');
+  Content.innerHTML = 'Hello! You are welcome.';
+}
+
+function doOnShowLocalStorage(e){
+  let s = '';Content.innerHTML = s;
+  for(let i=0;i<window.localStorage.length;i++){
+    let k = window.localStorage.key(i);
+    s = s + `<br>${k} = ${window.localStorage.getItem(k)}`;
+  }
+  Content.innerHTML = s;
 }
