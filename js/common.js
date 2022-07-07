d9698cd7dc8e567f59daea6f6f0574502520413c
uszn-zlt <108805592+uszn-zlt@users.noreply.github.com>
2022-07-07 15:58:17 +0300

    d

--- js/common.js
+++ js/common.js
@@ -16,3 +16,29 @@ function saveToFile(data, filename, type) {
     window.URL.revokeObjectURL(DataURL);
   }, 0);
 }
+
+function loadFromGithub(user,repo,filepath,token){
+  return new Promise((resolve,reject) => {
+    let Headers = {};
+    if(token){Headers.Authorization = 'token ' + token}
+    fetch(`https://api.github.com/repos/${user}/${repo}/contents/test001.txt`,{
+      method: 'Get',
+      headers: Headers
+    })
+    .then(resp => {
+      resolve(resp)
+    })
+    .catch(c => {
+      let s = 'Somthing wrong in loadFromGithub->fetch'
+      console.error(s);
+      reject(s);
+    })
+  })
+}
+
+function saveToGithub(data,user,repo,filepath,token){
+  loadFromGithub(user,repo,filepath,token)
+  .then(resp => {
+    console.log(resp);
+  })
+}
