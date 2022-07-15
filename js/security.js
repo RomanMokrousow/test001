d80fda6eaeb3cb0e8396f801aa58a98da3f3d237
uszn-zlt <108805592+uszn-zlt@users.noreply.github.com>
2022-07-15 16:43:45 +0500

    a

--- js/security.js
+++ js/security.js
@@ -2,7 +2,7 @@ export {encodeAES}
 
 async function encodeAES(srcArrayBuffer,key){
   let Result = {iv: new Uint8Array(16), data: null};
-  let local_key = (key == undefined)?getMasterkey:key;
+  let local_key = (key == undefined)?await getMasterkey():key;
   window.crypto.getRandomValues(Result.iv);
   Result.data = await window.crypto.subtle.encrypt({name: "AES-GCM", iv: Result.iv}, local_key, srcArrayBuffer);
   return Result;
